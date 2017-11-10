a5d239a794421466b91055f9781c02e7aca22fd6
seunghyukshin <sunghyuk9@naver.com>
2017-11-10 22:12:23 +0900

    fragment error

--- app/src/main/java/com/msproject/myhome/moara/MainActivity.java
+++ app/src/main/java/com/msproject/myhome/moara/MainActivity.java
@@ -3,27 +3,41 @@ package com.msproject.myhome.moara;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Fragment MainFragment;
+    private Fragment MyCouponFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             switch (item.getItemId()) {
-
+                case R.id.navigation_main:
+                    transaction.replace(R.id.navigation_main, MainFragment);
+                    break;
+                case R.id.navigation_myCoupon:
+                    transaction.replace(R.id.navigation_main, MyCouponFragment);
+                    break;
+                case R.id.navigation_myItem:
+                    break;
+                case R.id.navigation_setting:
+                    break;
+                default:
+                    break;
             }
             return false;
         }
 
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        MainFragment = new MainFragment();
+        MyCouponFragment = new MyCouponFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.content,MainFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+
     }
 
 }
