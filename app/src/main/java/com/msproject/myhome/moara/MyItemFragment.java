2f20c3ac0d5d3c65829e5fa58640e4083c31b594
godsangin <leein3293@naver.com>
2017-12-05 17:21:30 +0900

    datastructure modified

--- app/src/main/java/com/msproject/myhome/moara/MyItemFragment.java
+++ app/src/main/java/com/msproject/myhome/moara/MyItemFragment.java
@@ -2,6 +2,7 @@ package com.msproject.myhome.moara;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -23,6 +24,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 public class MyItemFragment extends Fragment {
@@ -120,8 +123,9 @@ public class MyItemFragment extends Fragment {
     }
 
     public void addAllItems(){
+
         DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference mConditionRef = mdatabase.child("/users/sampleuser/giftitem/");
+        DatabaseReference mConditionRef = mdatabase.child("/users/" + MainActivity.uid + "/giftitem/");
 
         mConditionRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
