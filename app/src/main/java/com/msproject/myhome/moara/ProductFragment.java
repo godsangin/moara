286ecd946e1e1414982048992e8ce2e42f67e3bd
MaengMaeng <maybe8189@naver.com>
2017-12-08 03:27:44 +0900

    Design Store Product

--- app/src/main/java/com/msproject/myhome/moara/ProductFragment.java
+++ app/src/main/java/com/msproject/myhome/moara/ProductFragment.java
@@ -1,55 +1,35 @@
 package com.msproject.myhome.moara;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.media.Image;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
-import static android.content.Context.MODE_PRIVATE;
-
-
 public class ProductFragment extends Fragment {
-    ImageView imageView;
-    EditText productName;
-    EditText cost;
-    Button submit;
-    Button cencel;
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    FirebaseStorage storage = FirebaseStorage.getInstance();
-    StorageReference storageReference = storage.getReference();
+    int REQUEST_CODE_ADD = 100;
+    int RESULT_CODE_ADD = 100;
+
+    int REQUEST_CODE_MODIFY = 101;
+
+    ListView productView;
+    StoreProductAdapter adapter;
+    Button addButton;
+
+    int index;
+
+    LinearLayout noProduct;
 
     public ProductFragment() {
         // Required empty public constructor
@@ -72,41 +52,62 @@ public class ProductFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product, container, false);
 
-        imageView = (ImageView) view.findViewById(R.id.imageView);
-        productName = (EditText)view.findViewById(R.id.productName);
-        cost = (EditText)view.findViewById(R.id.cost);
-        submit = (Button)view.findViewById(R.id.submit);
-        cencel = (Button)view.findViewById(R.id.cencel);
+        productView = (ListView) view.findViewById(R.id.ProductStoreList);
+        addButton = (Button) view.findViewById(R.id.addProduct);
+        noProduct = (LinearLayout) view.findViewById(R.id.NoProduct);
 
-        submit.setOnClickListener(new View.OnClickListener() {
+        noProduct.setVisibility(View.VISIBLE);
+
+        adapter= new StoreProductAdapter(inflater);
+
+        productView.setAdapter(adapter);
+
+        productView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onClick(View v) {
-                StorageReference logoRef = storageReference.child(mAuth.getCurrentUser().getUid() + "/logo.jpg");
-                imageView.setDrawingCacheEnabled(true);
-                imageView.buildDrawingCache();
-                Bitmap bitmap = imageView.getDrawingCache();
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                byte[] data = baos.toByteArray();
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                index = i;
+            }
+        });
 
-                UploadTask uploadTask = logoRef.putBytes(data);
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                    }
-                });
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCoupon();
             }
         });
         return view;
     }
 
+    public void addCoupon(){
+        Intent intent = new Intent(getActivity().getApplicationContext(), ModifyItem.class);
+
+        startActivityForResult(intent, REQUEST_CODE_ADD);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE_ADD){
+            if(resultCode == RESULT_CODE_ADD){
+                adapter.addItems(new StoreProduct("상품 이름", "상품 코스트","상품 유효기간","상품 정보"));
+                noProduct.setVisibility(View.INVISIBLE);
+
+                productView.setAdapter(adapter);
+                Toast.makeText(getActivity().getApplicationContext(), " 상품 추가 완료 " , Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getActivity().getApplicationContext(), " 상품 추가 실패 " , Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if(requestCode == REQUEST_CODE_MODIFY){
+            if(resultCode == RESULT_CODE_ADD){
+                Toast.makeText(getActivity().getApplicationContext(), " 상품 수정 완료 " , Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getActivity().getApplicationContext(), " 상품 수정 실패 " , Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -118,5 +119,87 @@ public class ProductFragment extends Fragment {
         super.onDetach();
     }
 
+    class StoreProduct{
+        String name;
+        String cost;
+        String validity;
+        String info;
+        int image;
+
+        public StoreProduct(String productName, String productCost, String productValidity, String productInfo){
+            this.name = productName;
+            this.cost = productCost;
+            this.validity = productValidity;
+            this.info = productInfo;
+            //this.image = image;
+        }
+        public String getName(){return this.name;}
+        public void setName(String name){ this.name = name; }
+
+        public String getCost(){return this.cost;}
+        public void setCost(String cost){this.cost = cost;}
+
+        public String getValidity(){return this.validity;}
+        public void setValidity(String validity){this.validity = validity;}
+
+        public String getInfo(){return this.info;}
+        public void setInfo(String info){this.info = info;}
+
+        public int getImage(){return this.image;}
+        public void setImage(int image){this.image = image;}
+    }
+
+    class StoreProductAdapter extends BaseAdapter {
+        ArrayList<StoreProduct> items;
+        LayoutInflater layoutInflater;
+
+        public StoreProductAdapter(LayoutInflater layoutInflater) {
+            this.items = new ArrayList<>();
+            this.layoutInflater = layoutInflater;
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public void addItems(StoreProduct item){items.add(item);}
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = layoutInflater.inflate(R.layout.store_item, null, false);
+            TextView name = (TextView) view.findViewById(R.id.store_product_name);
+            TextView cost = (TextView) view.findViewById(R.id.store_product_cost);
+            TextView validity = (TextView) view.findViewById(R.id.store_product_validity);
+            TextView info = (TextView) view.findViewById(R.id.store_product_info);
+
+            TextView modify = (TextView) view.findViewById(R.id.store_product_modify);
+
+            name.setText(items.get(position).getName());
+            cost.setText(items.get(position).getCost());
+            validity.setText(items.get(position).getValidity());
+            info.setText(items.get(position).getInfo());
+
+            modify.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity().getApplicationContext(), ModifyItem.class);
+
+                    startActivityForResult(intent, REQUEST_CODE_MODIFY);
+                }
+            });
+            return view;
+        }
+    }
 
 }
