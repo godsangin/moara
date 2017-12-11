bee1ee4d8ea972ce3d57ee8313867ea66ffcee23
godsangin <leein3293@naver.com>
2017-12-12 01:39:52 +0900

    function buy Coupon implements

--- app/src/main/java/com/msproject/myhome/moara/ShowCouponActivity.java
+++ app/src/main/java/com/msproject/myhome/moara/ShowCouponActivity.java
@@ -1,5 +1,6 @@
 package com.msproject.myhome.moara;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -10,6 +11,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -28,13 +30,18 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ShowCouponActivity extends AppCompatActivity {
     ListView listView;
     ProductAdapter adapter;
     FirebaseStorage storage = FirebaseStorage.getInstance();
     StorageReference storageRef = storage.getReference();
+
+    Context mContext;
+    LayoutInflater mLayoutInflater;
 
     int index;
 
@@ -53,13 +60,26 @@ public class ShowCouponActivity extends AppCompatActivity {
 
         adapter = new ProductAdapter();
 
+        mContext = this;
+        mLayoutInflater = getLayoutInflater();
+
+
         mConditionRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot snapshot: dataSnapshot.getChildren()){
                     adapter.addItems(new Product(snapshot.child("name").getValue().toString(), snapshot.child("price").getValue().toString(), snapshot.child("until").getValue().toString(), snapshot.child("comment").getValue().toString(), snapshot.child("imageSrc").getValue().toString()));
-                    listView.setAdapter(adapter);
+
                 }
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                        index = i;
+                        Product selectedItem = adapter.items.get(i);
+                        buyProduct(selectedItem);
+                    }
+                });
+                listView.setAdapter(adapter);
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -69,13 +89,7 @@ public class ShowCouponActivity extends AppCompatActivity {
 
 
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                index = i;
-                buyProduct();
-            }
-        });
+
     }
     public Drawable getDrawableFromBitmap(Bitmap bitmap){
         Drawable d = new BitmapDrawable(bitmap);
@@ -83,32 +97,17 @@ public class ShowCouponActivity extends AppCompatActivity {
     }
 
 
-    public void buyProduct(){
-        Product selectedProduct = (Product) adapter.getItem(index);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        ImageView imageView = null;
-        StorageReference islandRef = storageRef.child(selectedProduct.getImageSrc());
-        Glide.with(getApplicationContext()).using(new FirebaseImageLoader()).load(islandRef).into(imageView);
-        Bitmap bitmap = imageView.getDrawingCache(true);
-        builder.setTitle(selectedProduct.getName());
-        builder.setMessage("구매하시겠습니까? \n사용 쿠폰 : " + selectedProduct.getPrice() +" 개");
-        builder.setIcon(getDrawableFromBitmap(bitmap));
-
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialogI, int whichButton){
-                
-                Toast.makeText(getApplicationContext(), "구매 완료", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialogI, int whichButton){
-                Toast.makeText(getApplicationContext(), "구매 실패", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
+    public void buyProduct(Product selectedProduct){
+//        Product selectedProduct = (Product) adapter.getItem(index);
+        final BuyProductDialog buyProductDialog = new BuyProductDialog();
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        int year = calendar.get(Calendar.YEAR);
+        calendar.set(year+1, calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
+        String date = "~" + dateFormat.format(calendar.getTime());
+        selectedProduct.setUntil(date);
+        buyProductDialog.getInstance(mContext, mLayoutInflater, R.layout.activity_buy_product_dialog);
+        buyProductDialog.show(getApplicationContext(), selectedProduct);
     }
 
     class ProductAdapter extends BaseAdapter {
